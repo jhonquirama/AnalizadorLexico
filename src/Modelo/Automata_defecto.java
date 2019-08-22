ebd49a693276e7258bebf68553e6e0db575441a3
Jhon Quirama <quirama80@gmail.com>
2019-08-21 22:24:44 -0500

    automatas del primero a uno llamado vacio

---
+++ src/Modelo/Automata_defecto.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Automata_defecto {
+ int cont;
+    boolean aceptada;/*para guardar los caratcteres y los va ir separando*/
+
+    char[] car;
+
+    public void inicio() {
+        cont = 0;
+        aceptada = false;
+    }
+    
+     public void q0() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'd') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q1();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q1() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'e') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q2();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+ 
+     public void q2() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'f') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q3();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+      public void q3() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'e') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q4();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+       public void q4() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'c') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q5();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+       
+         public void q5() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 't') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                qF();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+public void qF() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'o') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                aceptada = true;
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+    }
+
---
+++ src/Modelo/Automata_derivarFuncion.java
@@ -0,0 +1,270 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Automata_derivarFuncion {
+    
+    int cont;
+    boolean aceptada;/*para guardar los caratcteres y los va ir separando*/
+
+    char[] car;
+
+    public void inicio() {
+        cont = 0;
+        aceptada = false;
+    }
+
+    public void q0() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'd') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q1();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q1() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'e') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q2();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q2() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'r') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q3();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q3() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'i') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q4();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q4() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'v') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q5();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q5() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'a') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q6();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q6() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'r') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q7();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q7() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'F') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q8();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q8() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'u') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q9();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q9() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'n') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q10();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q10() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'c') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q11();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+
+    public void q11() {
+
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'i') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                q12();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+    public void q12(){
+        if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'o') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                cont++;/*incrememnto mi contador*/
+
+                qF();
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+    
+      public void qF(){
+         if (cont < car.length) {/*cuantos espacios tiene mi arreglo*/
+
+            if (car[cont] == 'n') {/*el arreglo car en el contador 0 lo vamos a comparar si es = a*/
+
+                aceptada = true;
+
+            } else {
+
+                aceptada = false;
+
+            }
+        }
+    }
+}
